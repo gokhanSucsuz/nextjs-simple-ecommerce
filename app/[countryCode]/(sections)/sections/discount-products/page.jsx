d953e3ad1497262f72feea27a9bf63ebbcc9dcd5
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-07-14 22:58:03 +0300

    sort process is finished

--- app/[countryCode]/(sections)/sections/discount-products/page.jsx
+++ app/[countryCode]/(sections)/sections/discount-products/page.jsx
@@ -1,8 +1,31 @@
+import generateRandomProducts from '@/app/actions/generateRandomProducts'
+import ProductCard from '@/app/components/productCard'
+import ProductSection from '@/app/components/productSection'
 import React from 'react'
 
-const DiscountProducts = () => {
+const DiscountProducts = ({ params, searchParams }) => {
+      let discountProductsAll = generateRandomProducts.getDiscountProducts(20)
+      if (searchParams) {
+            if (searchParams.sortBy === "price_asc") {
+                  discountProductsAll = generateRandomProducts.sortAsc(discountProductsAll)
+            } else if (searchParams.sortBy === "price_desc") {
+                  discountProductsAll = generateRandomProducts.sortDesc(discountProductsAll)
+            } else {
+                  discountProductsAll = generateRandomProducts.getDiscountProducts(20)
+            }
+      }
       return (
-            <div>
+            <div className='flex flex-col'>
+                  <ProductSection showAll={false} title="Discount Products" className="mb-4" />
+                  <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+                        {
+                              discountProductsAll.map((item) => {
+                                    return (
+                                          <ProductCard product={item} locale={params.countryCode} />
+                                    )
+                              })
+                        }
+                  </div>
 
             </div>
       )
--- app/[countryCode]/(sections)/sections/weekly-products/page.jsx
+++ app/[countryCode]/(sections)/sections/weekly-products/page.jsx
@@ -1,8 +1,31 @@
+import generateRandomProducts from '@/app/actions/generateRandomProducts'
+import ProductCard from '@/app/components/productCard'
+import ProductSection from '@/app/components/productSection'
 import React from 'react'
 
-const WeeklyProducts = () => {
+const WeeklyProducts = ({ params, searchParams }) => {
+    let weeklyProductsAll = generateRandomProducts.getWeeklyProducts(1000)
+    if (searchParams) {
+        if (searchParams.sortBy === "price_asc") {
+            weeklyProductsAll = generateRandomProducts.sortAsc(weeklyProductsAll)
+        } else if (searchParams.sortBy === "price_desc") {
+            weeklyProductsAll = generateRandomProducts.sortDesc(weeklyProductsAll)
+        } else {
+            weeklyProductsAll = generateRandomProducts.getWeeklyProducts(1000)
+        }
+    }
     return (
-        <div>
+        <div className='flex flex-col'>
+            <ProductSection showAll={false} title="Weekly Products" className="mb-4" />
+            <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+                {
+                    weeklyProductsAll.map((item) => {
+                        return (
+                            <ProductCard product={item} locale={params.countryCode} />
+                        )
+                    })
+                }
+            </div>
 
         </div>
     )
